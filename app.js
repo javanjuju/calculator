007edb762bfe8f958bd0c581c42a365c9bf4ff92
javanjuju <105160680+javanjuju@users.noreply.github.com>
2023-03-19 17:26:13 +0000

    Add more functinality and styles

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const numbers = document.querySelectorAll('.number');
 const operands = document.querySelectorAll('.operands');
-const workingArea = document.querySelector('#workingArea').children; 
+const workingArea = document.querySelector('#workingArea').children;//where input is displayed. dived into three
 const answer = document.querySelector('#answer');
-const resultArea = document.querySelector('#resultArea');
+const resultArea = document.querySelector('#resultArea');//where results are displayed
+const clearBtn = document.querySelector('#clear');
+resultArea.textContent = 0;
 
-let firstNumber = 0;
-let secondNumber = 0;
-let operandIndicator = 0;
+let firstNumber = 0;//holds number before operand
+let secondNumber = 0;//holds number after operand
+let operandIndicator = 0;//shows if an operand has been input
 let workingChild = 0;
+let result = 0;
 let operand = workingArea[1].textContent;
 
+//functions for different operations 
 let sum = (a,b)=>{
     return a+b;
 }
@@ -25,39 +29,9 @@ let division = (a,b)=>{
 let modulus = (a,b)=>{
     return a%b;
 }
-
-numbers.forEach((number)=>{
-    number.addEventListener('click',e=>{
-        if(operandIndicator==0){
-            workingArea[workingChild].textContent = workingArea[workingChild].textContent + number.getAttribute('data-value');
-            firstNumber = workingArea[0].textContent;
-            console.log(`firstNumber ${firstNumber}`);
-            console.log(`secondNumber ${secondNumber}`);
-        }
-        else if(operandIndicator==1){
-            workingArea[workingChild].textContent = workingArea[workingChild].textContent + number.getAttribute('data-value');
-            secondNumber = workingArea[2].textContent;
-            console.log(`firstNumber ${firstNumber}`);
-            console.log(`secondNumber ${secondNumber}`);
-        }
-        
-    })
-})
-
-operands.forEach(operand=>{
-    operand.addEventListener('click',e=>{
-        workingChild=1;
-        operandIndicator=1;
-        workingArea[workingChild].textContent = operand.getAttribute('data-value');
-        workingChild = 2;
-        console.log(`firstNumber ${firstNumber}`);
-        console.log(`secondNumber ${secondNumber}`);
-    })
-})
-
-answer.addEventListener('click',e=>{
+let getAnswer = ()=>{
     let operand = workingArea[1].textContent
-    let result = 0;
+    result = 0;
     firstNumber = Number(firstNumber);
     secondNumber = Number(secondNumber);
     if(operand== '+'){
@@ -85,6 +59,56 @@ answer.addEventListener('click',e=>{
         result = String(result);
         resultArea.textContent=+result;
     }
+}
+
+//When a button with class number is clicked it displays
+numbers.forEach((number)=>{
+    number.addEventListener('click',e=>{
+        if(operandIndicator==0){
+            workingArea[workingChild].textContent = workingArea[workingChild].textContent + number.getAttribute('data-value');
+            firstNumber = workingArea[0].textContent;
+        }
+        else if(operandIndicator==1){
+            workingArea[workingChild].textContent = workingArea[workingChild].textContent + number.getAttribute('data-value');
+            secondNumber = workingArea[2].textContent;
+        }
+        
+    })
+})
+
+operands.forEach(operand=>{
+    operand.addEventListener('click',e=>{
+        if(operandIndicator == 0){
+            workingChild=1;
+            operandIndicator=1;
+            workingArea[workingChild].textContent = operand.getAttribute('data-value');
+            workingChild = 2;
+        }
+        else if(operandIndicator==1 && result==0){
+            //if the operand has been input and result is zero the answer is calculated
+            getAnswer();
+        }
+        else if(operandIndicator==1 && result!=0){
+            workingChild = 0;
+            workingArea[workingChild].textContent = result;
+            firstNumber = result;
+            result = 0;
+            workingChild=1;
+            operandIndicator=1;
+            workingArea[workingChild].textContent = operand.getAttribute('data-value');
+            workingChild = 2;
+            workingArea[workingChild].textContent = '';
+            resultArea.textContent = 0;
+            operandIndicator =1;
+        }
+    })
+})
+
+answer.addEventListener('click',e=>{
+    getAnswer();
+})
+clearBtn.addEventListener('click',e=>{
+    window.location.reload();
 })
 
 
